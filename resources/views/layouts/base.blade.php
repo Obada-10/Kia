3a74d5211f15d3a9fce2f453ea83339884386965
Obada-10 <Obadajlilate2001@gmail.com>
2024-09-02 10:57:15 +0200

    Header en Footer afgemaakt en Gestyled
    HTML-structuur aangemaakt
    CSS-styling toegepast

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -5,11 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Kia</title>
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 </head>
 <body>
     <header>
         <div class="logo">
-            <img src="{{ asset('img/logo.jpg') }}" alt="Logo">
+            <a href="/">KN</a>
         </div>
         <nav>
             <ul>
@@ -26,8 +27,32 @@
         @yield('content')
     </main>
 
-    <footer>
-        <p>&copy; 2024 Mijn Website. Alle rechten voorbehouden.</p>
-    </footer>    
+    <footer class="footer">
+        <div class="footer-container">
+            <div class="footer-block">
+                <h3>About Us</h3>
+                <p>Kia is een Zuid-Koreaans automerk dat deel uitmaakt van de Hyundai Motor Company.</p>
+                <p>(c) 2024 KIA.</p>
+            </div>
+            <div class="footer-block">
+                <h3>Menu</h3>
+                <ul>
+                    <li><a href="#">Modellen</a></li>
+                    <li><a href="#">Occassions</a></li>
+                    <li><a href="#">Zakelijk</a></li>
+                    <li><a href="#">Service</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </div>
+            <div class="footer-block">
+                <h3>Social</h3>
+                <ul>
+                    <li><a href="#facebook"><i class="fab fa-facebook-f"></i></a></li>
+                    <li><a href="#twitter"><i class="fab fa-twitter"></i></a></li>
+                    <li><a href="#instagram"><i class="fab fa-instagram"></i></a></li>
+                </ul>
+            </div>
+        </div>
+    </footer>       
 </body>
 </html>
